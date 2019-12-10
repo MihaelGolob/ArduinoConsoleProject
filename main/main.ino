f632948c9d1a8be8d009996e4d9ac8588b65003c
Mihael <golob.mihael@gmail.com>
2019-12-10 19:09:35 +0100

    initial commit

---
+++ main/main.ino
@@ -0,0 +1,13 @@
+#include <TFT_FastPin.h>
+#include <TFT_ILI9163.h>
+#include <User_Setup.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
